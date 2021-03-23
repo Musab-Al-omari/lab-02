2e02b679ab56c6594ae89dfffd4dfb804953a25c
Musab-Al-omari <musabsomari@gmail.com>
2021-03-23 21:58:49 +0200

    full lab

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 'use strict';
+let keywordArr = [];
+
 
 function Bringer(image_url, title, description, keyword, horns) {
     this.image_url = image_url;
@@ -7,41 +9,142 @@ function Bringer(image_url, title, description, keyword, horns) {
     this.keyword = keyword;
     this.horns = horns;
     Bringer.all.push(this)
+    // if (!keywordArr.includes(this.keyword)) {
+    if (keywordArr.includes(this.keyword) === false) { keywordArr.push(this.keyword) };
+    // }
 
 }
 Bringer.all = [];
+// console.log(keywordArr);
+// console.log(Bringer.all);
+
+
 
 Bringer.prototype.render = function () {
-    let workBox = $('<div></div>');
-    workBox.addClass(this.keyword);
-    let template = $('#photo-template').html();
-    workBox.html(template);
-    workBox.find('h2').text(this.title);
-    workBox.find('img').attr('src', this.image_url);
-    workBox.find('p').text(this.description);
-    $('main').append(workBox);
-
-
-
-    // let workBox = $('#photo-template').clone();
-    // workBox.addClass(this.keyword)
+    // let workBox = $('<div></div>');
+    // workBox.addClass(this.keyword);
+    // let template = $('#photo-template').html();
+    // workBox.html(template);
     // workBox.find('h2').text(this.title);
     // workBox.find('img').attr('src', this.image_url);
     // workBox.find('p').text(this.description);
-    // workBox.removeAttr('id');
     // $('main').append(workBox);
 
-}
-// function dropDown() {
-//     let myOptions = {};
-//     let mySelect = $('select');
-// $.each(myOptions, function(val, text) {
-//     mySelect.append($('<option></option>').val(val).html(text));
-// });
     
+    let workBox = $('#photo-template').clone();
+    workBox.addClass(this.keyword)
+    workBox.find('h2').text(this.title);
+    workBox.find('img').attr('src', this.image_url);
+    workBox.find('p').text(this.description);
+    workBox.removeAttr('id');
+    $('main').append(workBox);
+
+}
+
+let dropDown = function () {
+    for (let i = 0; i < keywordArr.length; i++) {
+        let myOptions = $('<option value="default">Filter by Keyword</option>');
+        $('#selectMenu').append(myOptions)
+        myOptions.attr('value', keywordArr[i])
+        myOptions.text(keywordArr[i])
+    }
+}
+
+
+
+let dropDownTow = function () {
+    let mySelect = $('<select id="selectMenuSecond"></select>');
+    $('header').append(mySelect)
+    let sortArry = ['sortBy', 'Title', 'NumberOfHorns']
+    for (let i = 0; i < sortArry.length; i++) {
+        let myOptions = $('<option value=""></option>');
+        mySelect.append(myOptions)
+
+        myOptions.attr('value', sortArry[i])
+        myOptions.text(sortArry[i])
+    }
+
+}
+
+
+
+
+let myEvent = function () {
+
+    $('#selectMenu').on('change', function () {
+        let selected = $(this).val();
+        console.log(selected);
+        $('section').hide();
+        if (selected === 'default') {
+            $('section').show();
+
+        } else {
+            $(`.${selected}`).fadeIn(800);
+        }
+
+    });
+}
+
+
+
+let myNewEvent = function () {
+
+
+    $('#selectMenuSecond').on('change', function () {
+        let newSelect = $(this).val();
+        console.log(newSelect);
+        if (newSelect === 'Title') {
+            // console.log("HELLO");
+            sortingByTitle()
+            $('section').html('');
+            Bringer.all.forEach(element => {
+                element.render();
+            });
+
+        } else if (newSelect === 'NumberOfHorns') {
+            sortByNumOfHorns();
+            $('#photo-template').html('');
+            Bringer.all.forEach(element => {
+                element.render();
+            });
+
+
+        }
+
+    })
+}
+
+
+// function sortingByTitle() {
+//     Bringer.all.sort(function (a, b) {
+//         var nameA = a.title
+//         var nameB = b.title
+//         if (nameA < nameB) {
+//             return -1;
+//         }
+//         if (nameA > nameB) {
+//             return 1;
+//         }
+
+//         return 0;
+//     });
+
 // }
 
+// function sortByNumOfHorns() {
+//     Bringer.all.sort(function (a, b) {
+//         var numA = a.horns
+//         var numB = b.horns
+//         if (numA < numB) {
+//             return -1;
+//         }
+//         if (numA > numB) {
+//             return 1;
+//         }
 
+//         return 0;
+//     });
+// }
 
 
 function getFromDate() {
@@ -56,12 +159,18 @@ function getFromDate() {
             let newWorkBox = new Bringer(element.image_url, element.title, element.description, element.keyword, element.horns);
             // console.log(newWorkBox);
             newWorkBox.render();
-        });
 
+        });
+        dropDown();
+        dropDownTow();
+        $('document').ready(myNewEvent);
+        $('document').ready(myEvent);
     })
-    
+
 }
 $('document').ready(getFromDate);
+
+
 
 
 
